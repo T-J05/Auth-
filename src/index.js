3f77e8350c40f1ef2bd35699e3509159ea2ffb04
Jose Toledo <bernardinotoledo6@gmail.com>
2024-10-31 15:55:06 -0300

    Se hicieron funciones del repositorio para el login

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
-import express from "express"
-import { PORT } from "./config"
+import express from "express";
+import { PORT } from "./config.js";
+import { UserRepo } from "./repositorio/userRepo.js";
+
+const app = express();
+app.set('view engine','ejs')
+app.use(express.json())
 
 
-const app = express()
-
-
-
-
-app.post("/registrar",(req,res)=>{
-    res.send("<h1>Hola mundo</h1>")
+app.get('/', (req,res) => {
+    res.render('inicio',{username: "Jose"})
 })
+app.post("/registrar",async (req,res)=>{
+    const {username,password,email} = req.body
+    console.log(req.body)
+    try{
+        const id = await UserRepo.create({username,password,email})
+        res.send({ id })
+    }catch(error){
+        // Normalmente no es buena idea mandar el error del repositorio
+        res.status(400).send(error.message)
+    }
+});
 
 
 app.post("/login",(req,res)=>{
     res.send("<h1>Hola mundo</h1>")
-})
+});
 
 app.post("/logout",(req,res)=>{
     res.send("<h1>Hola mundo</h1>")
-})
+});
 
 
 app.get("/vertodo",(req,res)=>{
     res.send("<h1>Hola mundo</h1>")
-})
+});
 
 
 
--- src/repositorio/userRepo.js
+++ src/repositorio/userRepo.js
@@ -1,6 +1,62 @@
 import DBLocal from "db-local"
+import crypto from 'node:crypto'
+import bcrypt from 'bcrypt'
+import { SALT_ROUDS } from "../config.js";
+
+
+const { Schema } = new DBLocal({path: './db'})
+
+const User = Schema('User',{
+    _id:{type: String,require:true},
+    username:{type: String,require:true},
+    password:{type: String,require:true},
+    password:{type: String,require:true}
+})
 
 export class UserRepo{
-    static create({username,password}) {}
-    static login({username, password}) {}
+    
+    static async create({username,password,email}) {
+        const validacionUser = new Validaciones(username,3);
+        validacionUser.validar();
+        
+        const validacionPassword = new Validaciones(password,6);
+        validacionPassword.validar();
+        
+        const validacionCorreo = new Validaciones(email,8);
+        validacionCorreo.validar();
+
+      // Asegurarse que el user no existe
+      const user = User.findOne({username})
+      if (user) throw new Error("El usuario ya existe")
+    
+      const id =  crypto.randomUUID()
+
+      // encriptar la contraseña 
+      const hashPassword = await bcrypt.hash(password,SALT_ROUDS) // hashsync bloquea el hilo principal
+      User.create({
+        _id: id,
+        username,
+        password: hashPassword
+      }).save()
+
+      return id
+    }
+    static login({username, password,email}){
+
+    }
+}
+
+class Validaciones{
+    constructor(parametro,minLetter){
+        this.parametro = parametro,
+        this.minLetter = minLetter;
+    }
+    validar(){
+        if(typeof this.parametro !== 'string'){
+            throw new Error(`El ${this.parametro} debe de ser un string`)
+        }
+        if (this.parametro.length < this.minLetter){ 
+            throw new Error(`El ${this.parametro} debe de tener al menos ${this.minLetter} letras`)
+        }
+    }
 }
